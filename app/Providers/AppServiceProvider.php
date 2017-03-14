cc9e6f5cdc592528664a839bcfb8f6084f16ac83
tier5 <work@tier5.us>
2017-03-14 14:27:52 +0000

    Setting cookie in Service Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->composer('layouts.app', function ($view){
+            $value = request()->cookie('name');
+            if(!$value){
+                /**Create a response instance*/
+               // $response = new Response($view);
+
+                /**Call the withCookie() method with the response method*/
+            //    $response->withCookie(cookie('name', 'value', $minutes));
+
+                /**To set the cookie forever*/
+               // $response->withCookie(cookie()->forever('name', 'value'));
+                //return the response
+           //     return $view->with('response',  $response);
+                $minutes =30;
+                return response($view)->cookie('name', 'value', $minutes);
+            
+           //     return $response;
+
+                    //Documentation
+            //     $cookie = cookie('name', 'value', $minutes, $path);
+            //    $view()->response()->cookie($cookie);
+            } else {
+                return $view;
+            }
+        });
     }
 
     /**
