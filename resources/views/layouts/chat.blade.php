2e4fd2802f5ea555fbe4d291b36b5e874648fbac
tier5 <work@tier5.us>
2017-03-22 12:56:33 +0000

    Changed design for the chat box

--- resources/views/layouts/chat.blade.php
+++ resources/views/layouts/chat.blade.php
@@ -1,73 +1,44 @@
-<!DOCTYPE html>
-<html >
-    <head>
-        <meta charset="UTF-8">
-        <title>Chat Box</title>  
-        <link rel="stylesheet" href="chat/css/style.css">
-    </head>
+    <!-- Chat Option -->
+    <section style="position: absolute; z-index: 100; bottom: : 0px; right: 0px;" >
+        <div id="live-chat">
+            <header class="clearfix">    
+                <a href="#" class="chat-close">x</a>
+                <h4>Admin</h4>
+                <span class="chat-message-counter">3</span>
+            </header>
 
-    <body>
-        <!doctype html>
-        <html lang="en">
-        
-            <head>
-                <meta charset="UTF-8">
-                <title>Live Chat</title>
-                <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
-            </head>
-
-            <body>
-                <div id="live-chat">
-                    <header class="clearfix">    
-                        <a href="#" class="chat-close">x</a>
-                        <h4>John Doe</h4>
-                        <span class="chat-message-counter">3</span>
-                    </header>
-
-                    <div class="chat">            
-                        <div class="chat-history">
-                            <div class="chat-message clearfix">
-                                <img src="http://lorempixum.com/32/32/people" alt="" width="32" height="32">
-                                <div class="chat-message-content clearfix">
-                                    <span class="chat-time">13:35</span>
-                                    <h5>John Doe</h5>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, explicabo quasi ratione odio dolorum harum.</p>
-                                </div> <!-- end chat-message-content -->
-                            </div> <!-- end chat-message -->
-                            <hr>
-                            <div class="chat-message clearfix">
-                                <img src="http://gravatar.com/avatar/2c0ad52fc5943b78d6abe069cc08f320?s=32" alt="" width="32" height="32">
-                                <div class="chat-message-content clearfix">
-                                    <span class="chat-time">13:37</span>
-                                    <h5>Marco Biedermann</h5>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, nulla accusamus magni vel debitis numquam qui tempora rem voluptatem delectus!</p>
-                                </div> <!-- end chat-message-content -->
-                            </div> <!-- end chat-message -->
-                            <hr>
-                            <div class="chat-message clearfix">
-                                <img src="http://lorempixum.com/32/32/people" alt="" width="32" height="32">
-                                <div class="chat-message-content clearfix">
-                                    <span class="chat-time">13:38</span>
-                                    <h5>John Doe</h5>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
-                                </div> <!-- end chat-message-content -->
-                            </div> <!-- end chat-message -->
-                            <hr>
-                        </div> <!-- end chat-history -->
-                        <p class="chat-feedback">Your partner is typing…</p>
-                        <form action="#" method="post">
-                            <fieldset>
-                                <input type="text" placeholder="Type your message…" autofocus>
-                                <input type="hidden">
-                            </fieldset>
-                        </form>
-                    </div> <!-- end chat -->
-                </div> <!-- end live-chat -->
-
-                <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-            </body>
-        </html>
-      
-        <script src="chat/js/index.js"></script>
-    </body>
-</html>
+            <div class="chat">            
+                <div class="chat-history">
+                    <div class="chat-message clearfix">
+                        <img src="chat/user-logo.png" alt="" width="32" height="32"> 
+                        <div class="chat-message-content clearfix">
+                            <span class="chat-time" style="color: #18BC9C">13:35</span>
+                            <h5 style="color: #18BC9C">John Doe</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, explicabo quasi ratione odio dolorum harum.</p>
+                        </div> <!-- end chat-message-content -->
+                    </div> <!-- end chat-message -->
+                    <hr style="color: black; height: 1px;">
+                    <div class="chat-message clearfix">
+                        <img src="chat/computer-user.png" alt="" width="32" height="32">
+                        <div class="chat-message-content clearfix">
+                            <span class="chat-time"style="color: #18BC9C">13:37</span>
+                            <h5 style="color: #18BC9C">Admin</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, nulla accusamus magni vel debitis numquam qui tempora rem voluptatem delectus!</p>
+                        </div> <!-- end chat-message-content -->
+                    </div> <!-- end chat-message -->
+                    <hr style="color: black; height: 1px;">
+                </div> <!-- end chat-history -->
+                <br>
+                <p class="chat-feedback" style="color: #18BC9C;">Admin is typing…</p>
+                <form action="#" method="post">
+                    <fieldset>
+                        <textarea type="text" placeholder="Type your message…" autofocus  style="color: #9a9a9a"></textarea>
+                        <button class="btn btn-success pull-right" type="submit" style="margin-top: 2px; padding: 6px 6px 6px 6px;">
+                            Send
+                        </button>
+                        <input type="hidden">
+                    </fieldset>
+                </form>
+            </div> <!-- end chat -->
+        </div> <!-- end live-chat -->
+    </section>
